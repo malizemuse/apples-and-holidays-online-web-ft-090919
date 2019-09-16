031701b1c04c48ac8ead35c7a377e432235acbdd
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-16 00:48:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,8 +28,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   
+  new_holiday_hash =
+  
   holiday_hash[:winter].each do |holiday| 
     holiday << supply
+    
   end 
   
   holiday_hash
